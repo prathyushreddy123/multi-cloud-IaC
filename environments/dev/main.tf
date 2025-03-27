62e72c45bfa6b0f2f0b4562cf9eff96a16fbea69
Prathyush Dommata <prathyushdommata@gmail.com>
2025-03-27 16:15:29 -0500

    dev tf configuration

---
+++ environments/dev/main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source         = "../../modules/vpc"
+  cidr_block     = "10.0.0.0/16"
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  environment    = var.environment
+}
+
+module "kms" {
+  source = "../../modules/kms"
+}
+
+module "iam" {
+  source      = "../../modules/iam"
+  environment = var.environment
+}
+
+module "eks" {
+  source           = "../../modules/eks"
+  subnet_ids       = module.vpc.public_subnet_ids
+  cluster_role_arn = module.iam.eks_cluster_role_arn
+  environment      = var.environment
+}
